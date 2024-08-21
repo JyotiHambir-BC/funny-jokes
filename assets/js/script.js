145dd25a71abf85246bc0839b2a5d56502ac55d8
Jyoti Hambir <qdde3t1v@students.codeinstitute.net>
2024-08-21 10:38:57 +0000

    add another way for write json code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,16 +29,30 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke();
 
-function generateJoke() {
+// USING async/await
+async function generateJoke() {
 
     const config = {
         headers: {
             Accept: 'application/json',
         },
 }
-    fetch('https://icanhazdadjoke.com/', config)
-    .then(res => res.json())
-    .then(data => {
-        jokeElement.innerHTML = data.joke
-    });
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+    const data = await res.json()
+
+    jokeElement.innerHTML = data.joke
 }
+// USING .then()
+// function generateJoke() {
+
+//     const config = {
+//         headers: {
+//             Accept: 'application/json',
+//         },
+// }
+//     fetch('https://icanhazdadjoke.com/', config)
+//     .then(res => res.json())
+//     .then(data => {
+//         jokeElement.innerHTML = data.joke
+//     });
+// }
